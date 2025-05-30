1a2f9e2efa840464763021a14a61ae976a58f64e
TomaszOrda <tomaszorda@outlook.com>
2025-05-30 21:14:27 +0200

    Add a functionality for splitting input into lines

---
+++ solutions/utils.cpp
@@ -0,0 +1,19 @@
+#include <string_view>
+#include <vector>
+#include <iostream>
+
+std::vector<std::string_view> split_lines(std::string_view text){
+    std::vector<std::string_view> result{};
+    size_t line_start = 0;
+    for (size_t i{0}; i<text.length(); i++){
+        char c = text[i];
+        if (c == '\n'){
+            result.push_back(text.substr(line_start, i-line_start));
+            line_start = i;
+        }
+    }
+    if (line_start != text.length()){
+        result.push_back(text.substr(line_start, text.length() - line_start));
+    }
+    return result;
+}
