a81aea9e84d03c1d7bcd5f31fa0e200b0eea69da
TomaszOrda <tomaszorda@outlook.com>
2025-05-31 11:40:26 +0200

    Add solution for day 1 part 2

--- README.md
+++ README.md
@@ -11,11 +11,11 @@ To run the solution:
 
 Data for each task can be accessed through the site.
 
-1/50⭐
+2/50⭐
 
 |            | 日 | 月 | 火 | 水 | 木 | 金 | 土 |
 |:-----------|-------|-------|-------|-------|-------|-------|-------|
-| **Week 1** |⭐&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|
+| **Week 1** |⭐&nbsp;⭐|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|
 | **Week 2** |&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|
 | **Week 3** |&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;|
 | **Week 4** |&nbsp;|&nbsp;|&nbsp;|&nbsp;|&nbsp;&nbsp;|||
--- solutions/day_1/day_1_part_2.cpp
+++ solutions/day_1/day_1_part_2.cpp
@@ -1,15 +1,55 @@
 #include "../runner_oo.hpp"
+#include "../utils.cpp"
 #include <string>
 #include <optional>
 
+int starts_with_digit(std::string_view s, bool reverse=false){
+    auto pattern_matcher = [reverse](std::string_view s, std::string_view pattern){
+        return reverse ? s.ends_with(pattern) : s.starts_with(pattern);
+    };
+    char first_character = reverse ? s.back() : s[0];
+    if ('0' <= first_character && first_character <= '9'){
+        return first_character - '0';
+    }else{
+        std::string_view digits[10] 
+            {"zero", "one", "two", 
+            "three", "four", "five", 
+            "six", "seven", "eight", "nine"};
+        for (size_t i{0}; i < 10; i++){
+            std::string_view pattern = digits[i];
+            if (s.length() >= pattern.length() && pattern_matcher(s, pattern)){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
 std::optional<std::string> solve(std::string_view input){
-    return std::nullopt;
+    int sum_of_calibraction_values = 0;
+    for (std::string_view line : split_lines(input)){
+        int first_digit {-1};
+        int last_digit {-1};
+
+        for (size_t i=1; first_digit==-1 or last_digit==-1; i++){
+            size_t i_back { line.length()-i };
+            if (first_digit==-1){
+                first_digit = starts_with_digit(line.substr(0,i), true);
+            }
+            if (last_digit==-1){
+                last_digit = starts_with_digit(line.substr(i_back,i));
+            }
+        }
+        int calibration_value = 10*first_digit + last_digit;
+        sum_of_calibraction_values += calibration_value;
+    }
+    return std::to_string(sum_of_calibraction_values);
 }
 
 int main(int argc, char *argv[]) {
     Solution solution = Solution(1, 2, solve);
     if (argc > 1){
-        return solution.test_run();
+        return solution.run("day_1_test_2.txt");
     }else{
         return solution.run();
     }
