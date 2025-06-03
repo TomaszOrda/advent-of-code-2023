d4a74d733ab517f78528dc3b765ed5468bb29d7f
TomaszOrda <tomaszorda@outlook.com>
2025-06-03 17:54:50 +0200

    Add split functionality for string delimiters

--- solutions/utils.cpp
+++ solutions/utils.cpp
@@ -1,24 +1,33 @@
 #include "utils.hpp"
+#include <string>
 #include <string_view>
 #include <vector>
 #include <charconv>
 
+#include<iostream>
 //The logic could be optimized by the use of iterators.
 //This could save the overhead of holding a vector of string_views.
-std::vector<std::string_view> split(std::string_view text, char delimiter){
+std::vector<std::string_view> split(std::string_view text, std::string_view delimiter){
     std::vector<std::string_view> result{};
+    if (text.length() == 0){
+        return result;
+    }
     size_t line_start = 0;
-    for (size_t i{0}; i<text.length(); i++){
-        char c = text[i];
-        if (c == delimiter){
-            result.push_back(text.substr(line_start, i-line_start));
-            line_start = i+1;
-        }
-    }
-    if (line_start != text.length()){
-        result.push_back(text.substr(line_start, text.length() - line_start));
-    }
+    while (true){
+        size_t line_end = text.find(delimiter, line_start);
+        result.push_back(text.substr(line_start, line_end - line_start));
+        if (line_end <= text.length()+ delimiter.length()){
+            line_start = line_end + delimiter.length();
+            //Make sure that line_start does not roll over. 
+        }else{
+            break;
+        };
+    };
     return result;
+}
+std::vector<std::string_view> split(std::string_view text, char delimiter){
+    std::string_view sv_delimiter(&delimiter, 1);
+    return split(text, sv_delimiter);
 }
 
 std::vector<std::string_view> split_lines(std::string_view text){
--- solutions/utils.hpp
+++ solutions/utils.hpp
@@ -4,6 +4,7 @@
 
 std::vector<std::string_view> split_lines(std::string_view text);
 std::vector<std::string_view> split(std::string_view text, char delimiter);
+std::vector<std::string_view> split(std::string_view text, std::string_view delimiter);
 int svtoi(std::string_view s);
 
 template<typename T>
