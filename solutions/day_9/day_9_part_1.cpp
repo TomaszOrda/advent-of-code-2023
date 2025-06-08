cc13355f2bc4e99979607247c4b114c340414f78
TomaszOrda <tomaszorda@outlook.com>
2025-06-08 11:08:57 +0200

    Use recursion instead of remembering sequences in a vector

--- solutions/day_9/day_9_part_1.cpp
+++ solutions/day_9/day_9_part_1.cpp
@@ -6,67 +6,51 @@
 #include <numeric>
 #include <vector>
 
-class Sequence{
+class ValueReport{
     std::vector<int> sequence;
 public:
-    Sequence(std::vector<int> sequence) : sequence{sequence}
+    ValueReport(std::vector<int> sequence) : sequence{sequence}
     {}
-    Sequence(std::string_view sequence_record) 
+    ValueReport(std::string_view sequence_record) 
         : sequence{std::ranges::to<std::vector<int>>(split(sequence_record, ' ') | std::views::transform(svtoi))}
     {}
-    bool all_zero() const{
+    bool all_zeros() const{
         return std::all_of(sequence.begin(), sequence.end(), [](auto x){return x==0;});
     }
-    Sequence next(){
+    ValueReport next() const{
         std::vector<int> result{};
         for (size_t i{1}; i<sequence.size(); i++) {
             result.push_back(sequence[i] - sequence[i-1]);
         }
-        return Sequence(result);
+        return ValueReport(result);
     }
-    int prediction(int next_sequence_prediction) const{
-        return sequence.back() + next_sequence_prediction;
-    }
-};
-
-class OasisValueReport{
-    std::vector<Sequence> sequences;
-public:
-    OasisValueReport(std::string_view record) : sequences{std::vector<Sequence>{Sequence(record)}}
-    {}
-    int prediction(){
-        while(!sequences.back().all_zero()){
-            sequences.push_back(
-                sequences.back().next()
-            );
+    int prediction() const{
+        if (all_zeros()){
+            return 0;
         }
-        int current_sequence_prediction = 0;
-        for (const auto& sequence : sequences | std::views::reverse | std::views::drop(1) ){
-            current_sequence_prediction = sequence.prediction(current_sequence_prediction);
-        }
-        return current_sequence_prediction;
+        return sequence.back() + next().prediction();
     }
 };
 
 class OasisReport{
-    std::vector<OasisValueReport> reports{};
+    std::vector<ValueReport> reports{};
 public:
     OasisReport(std::string_view record){
         for (const auto& line : split_lines(record)){
-            reports.push_back(OasisValueReport(line));
+            reports.push_back(ValueReport(line));
         }
     }
-    int sum_of_extrapolated_values(){
+    int sum_of_predictions(){
         return std::accumulate(
             reports.begin(), 
             reports.end(), 
             0, 
-            [](int a, OasisValueReport b){return a + b.prediction();});
+            [](int a, ValueReport& b){return a + b.prediction();});
     }
 };
 
 std::optional<std::string> solve(std::string_view input){
-    return std::to_string(OasisReport(input).sum_of_extrapolated_values());
+    return std::to_string(OasisReport(input).sum_of_predictions());
 }
 
 int main(int argc, [[maybe_unused]] char *argv[]) {
--- solutions/day_9/day_9_part_2.cpp
+++ solutions/day_9/day_9_part_2.cpp
@@ -6,69 +6,44 @@
 #include <numeric>
 #include <vector>
 
-class Sequence{
+class ValueReport{
     std::vector<int> sequence;
 public:
-    Sequence(std::vector<int> sequence) : sequence{sequence}
+    ValueReport(std::vector<int> sequence) : sequence{sequence}
     {}
-    Sequence(std::string_view sequence_record) 
+    ValueReport(std::string_view sequence_record) 
         : sequence{std::ranges::to<std::vector<int>>(split(sequence_record, ' ') | std::views::transform(svtoi))}
     {}
-    bool all_zero() const{
+    bool all_zeros() const{
         return std::all_of(sequence.begin(), sequence.end(), [](auto x){return x==0;});
     }
-    Sequence next(){
+    ValueReport next() const{
         std::vector<int> result{};
         for (size_t i{1}; i<sequence.size(); i++) {
             result.push_back(sequence[i] - sequence[i-1]);
         }
-        return Sequence(result);
+        return ValueReport(result);
     }
-    int prediction(int next_sequence_prediction) const{
-        return sequence.back() + next_sequence_prediction;
+    int prediction() const{
+        if (all_zeros()){
+            return 0;
+        }
+        return sequence.back() + next().prediction();
     }
-    int extrapolate_backwards(int next_sequence_extrapolation) const{
-        return sequence[0] - next_sequence_extrapolation;
-    }
-};
-
-class OasisValueReport{
-    std::vector<Sequence> sequences;
-public:
-    OasisValueReport(std::string_view record) : sequences{std::vector<Sequence>{Sequence(record)}}
-    {}
-    int prediction(){
-        while(!sequences.back().all_zero()){
-            sequences.push_back(
-                sequences.back().next()
-            );
+    int extrapolate_backwards() const{
+        if (all_zeros()){
+            return 0;
         }
-        int current_sequence_prediction = 0;
-        for (const auto& sequence : sequences | std::views::reverse | std::views::drop(1) ){
-            current_sequence_prediction = sequence.prediction(current_sequence_prediction);
-        }
-        return current_sequence_prediction;
-    }
-    int extrapolate_backwards(){
-        while(!sequences.back().all_zero()){
-            sequences.push_back(
-                sequences.back().next()
-            );
-        }
-        int current_sequence_extrapolation = 0;
-        for (const auto& sequence : sequences | std::views::reverse | std::views::drop(1) ){
-            current_sequence_extrapolation = sequence.extrapolate_backwards(current_sequence_extrapolation);
-        }
-        return current_sequence_extrapolation;
+        return sequence[0] - next().extrapolate_backwards();
     }
 };
 
 class OasisReport{
-    std::vector<OasisValueReport> reports{};
+    std::vector<ValueReport> reports{};
 public:
     OasisReport(std::string_view record){
         for (const auto& line : split_lines(record)){
-            reports.push_back(OasisValueReport(line));
+            reports.push_back(ValueReport(line));
         }
     }
     int sum_of_predictions(){
@@ -76,14 +51,14 @@ public:
             reports.begin(), 
             reports.end(), 
             0, 
-            [](int a, OasisValueReport b){return a + b.prediction();});
+            [](int a, ValueReport& b){return a + b.prediction();});
     }
     int sum_of_extrapolated_values(){
         return std::accumulate(
             reports.begin(), 
             reports.end(), 
             0, 
-            [](int a, OasisValueReport b){return a + b.extrapolate_backwards();});
+            [](int a, ValueReport& b){return a + b.extrapolate_backwards();});
     }
 };
 
